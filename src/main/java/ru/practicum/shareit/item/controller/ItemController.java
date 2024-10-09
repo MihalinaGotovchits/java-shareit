3e4fec2d8d9b523e23a7a6f6e7249b7b00150480
MihalinaGotovchits <Mix-Mihalina@ya.ru>
2024-10-09 14:45:55 +0300

    Выполнила реализацию ТЗ 14

---
+++ src/main/java/ru/practicum/shareit/item/controller/ItemController.java
@@ -0,0 +1,52 @@
+package ru.practicum.shareit.item.controller;
+
+import lombok.AllArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.*;
+import ru.practicum.shareit.item.dto.ItemDto;
+import ru.practicum.shareit.item.service.ItemService;
+import ru.practicum.shareit.utils.Create;
+
+import java.util.List;
+
+@Slf4j
+@RestController
+@RequestMapping("/items")
+@AllArgsConstructor
+public class ItemController {
+    private final ItemService itemService;
+
+    @GetMapping("/{itemId}")
+    public ItemDto getItem(@PathVariable Long itemId) {
+        log.info("GET / items / {}", itemId);
+        return itemService.getItemById(itemId);
+    }
+
+    @GetMapping
+    public List<ItemDto> getItemsByOwner(@RequestHeader("X-Sharer-User-Id") Long userId) {
+        log.info("GET / items");
+        return itemService.getItemByOwner(userId);
+    }
+
+    @GetMapping("/search")
+    public List<ItemDto> getItemsBySearch(@RequestParam String text) {
+        log.info("GET / search / {}", text);
+        return itemService.getItemBySearch(text);
+    }
+
+    @PostMapping
+    public ItemDto save(@Validated(Create.class) @RequestBody ItemDto itemDto,
+                        @RequestHeader("X-Sharer-User-Id") Long userId) {
+        log.info("POST / items / {}, {}", itemDto.getName(), userId);
+        return itemService.save(itemDto, userId);
+    }
+
+    @PatchMapping("{itemId}")
+    public ItemDto update(@PathVariable Long itemId, @RequestBody ItemDto itemDto,
+                          @RequestHeader("X-Sharer-User-Id") Long userId) {
+        log.info("PATCH / items / {}", itemId);
+        return itemService.update(itemId, itemDto, userId);
+    }
+}
+
